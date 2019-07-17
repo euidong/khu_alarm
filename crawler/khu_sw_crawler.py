79fdcf9681cb8537ccfb22eaa6fb5ac151feaf76
euidong <justicedong@naver.com>
2019-07-17 17:06:15 +0900

    sw crawling 추가

--- crawler/khu_sw_crawler.py
+++ crawler/khu_sw_crawler.py
@@ -3,8 +3,30 @@ from bs4 import BeautifulSoup
 from time import sleep
 import pymysql.cursors
 
-if __name__=="__main__":
+
+def update_db(number, name, date, url, minus):
     connection = pymysql.connect(host = 'localhost', port = 3306, user='root', passwd='1879', db = 'khu_alarm', charset='utf8')
+    try :
+        with connection.cursor() as cursor :
+            # 게시물 삽입
+            sql = "INSERT INTO `khu_sw_notice` (`id`, `name`, `date`, `url`) VALUES (%s,%s,%s,%s)"
+            cursor.execute(sql,(number, name, date, url))
+            connection.commit()
+
+            if minus:
+                sql = "DELETE FROM `khu_sw_notice` WHERE `id` > %s"
+                cursor.execute(sql, str(-int(url[-3:]) + 100))
+                connection.commit()
+            else :
+                sql = "DELETE FROM `khu_sw_notice` WHERE `id` < %s AND `id` > 0"
+                cursor.execute(sql, str(int(number) - 100))
+                connection.commit()
+    finally:
+        connection.close()
+
+
+if __name__=="__main__":
+    
     
     while True :
         f = open("khu_sw_file.txt", mode ="rt", encoding="utf-8")
@@ -23,28 +45,27 @@ if __name__=="__main__":
             name = tr.find('div',class_='bo_tit').a.get_text().strip()
             date = tr.find('td',class_='td_datetime').get_text().strip()
             url = tr.find('div', class_='bo_tit').a["href"]
-
-            print(number)
-            print(name)
-            print(date)
-            print(url)
-            # if number == '공지' :
             
-            # else :
-            #     if RECENT_POST_NUMBER < number :
-            #         if RESULT_POST_NUMBER < number :
-            #             RESULT_POST_NUMBER = number
-            #     try :
-            #         with connection.cursor() as cursor :
-            #             # 게시물 삽입
-            #             sql = "INSERT INTO `khu_sw_notice` (`id`, `name`, `date`, `url`) VALUES (%s,%s,%s,%s)"
-            #             cursor.execute(sql,(number, name, date, url))
-            #             connection.commit()
+            post_number = url[-3:]
+            if post_number > RECENT_POST_NUMBER:
+                print(number)
+                print(name)
+                print(date)
+                print(url)
+                if RESULT_POST_NUMBER < post_number :
+                    RESULT_POST_NUMBER = post_number
 
-            #             # 게시물 100개 이상이면 삭제
-            #             sql = "DELETE FROM `khu_sw_notice` WHERE `id` = %s"
-            #             cursor.execute(sql, str(NUMBER - 100))
-            #             connection.commit()
-            #     finally:
-            #         connection.close()
-        # sleep(10)
+                if number == '공지' :
+                    number = '-' + post_number
+                    update_db(number, name, date, url, True)
+                
+                else :
+                     update_db(number, name, date, url, False)
+
+        if RESULT_POST_NUMBER == RECENT_POST_NUMBER:
+            print("NOT Changed")
+
+        with open('khu_sw_file.txt', 'w') as result:
+            result.write(RESULT_POST_NUMBER)
+        
+        sleep(10)
--- notice/views.py
+++ notice/views.py
@@ -1,4 +1,4 @@
-from .models import Khu_ce_notice, Personal_notice
+from .models import Khu_ce_notice, Khu_sw_notice, Personal_notice
 from .serializers import Ce_noticeSerializer, Personal_noticeSerializer
 from django.shortcuts import render, redirect
 from rest_framework import generics
@@ -15,7 +15,8 @@ class noticeList(APIView):
     def get(self, request, format=None):
         if request.user.is_authenticated:
             ce_notice = Khu_ce_notice.objects.all()
-            context ={'context': ce_notice}
+            sw_notice = Khu_sw_notice.objects.all()
+            context ={'ce_notice': ce_notice, 'sw_notice': sw_notice}
             return render(request, 'notice.html', context)
         else :
             return redirect('/')
@@ -29,9 +30,16 @@ class noticeList(APIView):
                 if key != 'csrfmiddlewaretoken':
                     if key[:2] == 'ce':
                         try :
-                            Personal_notice.objects.get(userId_id= request.user.id, noticeId=key[2:])
+                            Personal_notice.objects.get(userId_id=request.user.id, noticeId=key[2:])
                         except ObjectDoesNotExist:
                             personal_notice =Personal_notice(userId_id=request.user.id, siteId=0, noticeId=key[2:])
+                            personal_notice.save()
+                        return redirect('./total')
+                    elif key[:2] == 'sw':
+                        try :
+                            Personal_notice.objects.get(userId_id=request.user.id, noticeId=key[2:])
+                        except ObjectDoesNotExist:
+                            personal_notice =Personal_notice(userId_id=request.user.id, siteId=1, noticeId=key[2:])
                             personal_notice.save()
                         return redirect('./total')
             return redirect('/')
@@ -48,8 +56,11 @@ class myNoticeList(APIView):
             personal_notice = Personal_notice.objects.filter(userId_id=request.user.id)
             result_notice = []
             for checker in personal_notice :
-                result_notice.append(Khu_ce_notice.objects.get(id=checker.noticeId))
-            context ={'context': result_notice}
+                if checker.siteId == 0:
+                    result_notice.append(Khu_ce_notice.objects.get(id=checker.noticeId))
+                elif checker.siteId == 1:
+                    result_notice.append(Khu_sw_notice.objects.get(id=checker.noticeId))
+            context ={'result_notice': result_notice}
             return render(request, 'my_notice.html', context)
         else :
             return redirect('/')
@@ -60,9 +71,8 @@ class myNoticeList(APIView):
         else:
             for key in request.data:
                 if key != 'csrfmiddlewaretoken':
-                    if key[:2] == 'ce':
-                        delete_notice =Personal_notice.objects.get(userId_id=request.user.id, noticeId = key[2:])
-                        delete_notice.delete()
-                        return redirect('./my')
+                    delete_notice =Personal_notice.objects.get(userId_id=request.user.id, noticeId = key[2:])
+                    delete_notice.delete()
+                    return redirect('./my')
             return redirect('/')
             
